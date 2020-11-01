f90ed4175bef8b0332dca5cec79ca8ee8e8d54f7
Ivan Kisyov <dev.vanks@gmail.com>
2020-11-01 19:06:42 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gtw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'guess-the-word';
+}
