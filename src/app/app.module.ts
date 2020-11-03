384d3aed2474f5f106d007ab505a90d0e8daadeb
Ivan Kisyov <dev.vanks@gmail.com>
2020-11-03 09:50:24 +0200

    Setup store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UiMaterialModule } from './shared/ui/ui-material/ui-material.module';
 import { CoreModule } from './core/core.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, BrowserAnimationsModule, UiMaterialModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    UiMaterialModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
